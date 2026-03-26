8fb6c7a5003bc9aa1470591bd49a3d790d8019b4
PS44142 Quach Thanh Danh <chanthienq640@gmail.com>
2026-03-26 15:43:03 +0700

    tao file index.html

---
+++ Project_Backend/slide4/src/components/bai1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Calculator: 04 - Quach Thanh Danh</h1>
+    <h3>Count {{ count }}</h3>
+
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => count.value++
+const decrement = () => count.value--
+const reset = () => (count.value = 0)
+</script>
+
+<style lang="scss" scoped>
+button {
+  margin: 5px;
+  padding: 8px 12px;
+}
+</style>
